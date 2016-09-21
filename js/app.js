da6a34f7fbaf22365b37c00be9e81a7017cd16dc
Chaitalee <chaitalee.wohlig@gmail.com>
2016-09-21 12:13:54 +0530

    landing page

--- js/app.js
+++ js/app.js
@@ -198,7 +198,7 @@ firstapp.config(function($stateProvider, $urlRouterProvider, $httpProvider, $loc
             templateUrl: "views/template.html",
             controller: 'WorldTourInsideCtrl'
         });
-    $urlRouterProvider.otherwise("/home");
+    $urlRouterProvider.otherwise("/landing");
     $locationProvider.html5Mode(isproduction);
 });
 
