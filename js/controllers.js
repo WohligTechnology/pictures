92bf666bcc89b2eb412f419f7b4fad448df5ed35
sukshawohlig <sukshawohlig@gmail.com>
2016-12-12 11:56:41 +0530

    changes

--- js/controllers.js
+++ js/controllers.js
@@ -202,7 +202,28 @@ angular.module('phonecatControllers', ['templateservicemod', 'navigationservice'
 
         }, 1000);
     });
-
+    $scope.$on('$viewContentLoaded', function() {
+           setTimeout(function() {
+               (function(d, s, id) {
+                   var js, fjs = d.getElementsByTagName(s)[0];
+                   if (d.getElementById(id)) return;
+                   js = d.createElement(s);
+                   js.id = id;
+                   js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.4&appId=1452795161694777";
+                   fjs.parentNode.insertBefore(js, fjs);
+               }(document, 'script', 'facebook-jssdk'));
+               ! function(d, s, id) {
+                   var js, fjs = d.getElementsByTagName(s)[0],
+                       p = /^http:/.test(d.location) ? 'http' : 'https';
+                   if (!d.getElementById(id)) {
+                       js = d.createElement(s);
+                       js.id = id;
+                       js.src = p + "://platform.twitter.com/widgets.js";
+                       fjs.parentNode.insertBefore(js, fjs);
+                   }
+               }(document, "script", "twitter-wjs");
+           }, 500);
+       });
     $scope.getin = {};
     $scope.getin.enquiryarr = [];
     $scope.showThanks = false;
